369d44aebae37df029f224bd417d1c481e12b9fe
Gwendoline53 <Gwendolinecadet73@gmail.com>
2025-06-17 07:51:04 +0200

    Formulaire de contact visuel

---
+++ src/Controller/FormulaireController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class FormulaireController extends AbstractController
+{
+    #[Route('/contact', name: 'app_formulaire')]
+    public function index(): Response
+    {
+        return $this->render('formulaire/formulaire.html.twig');
+    }
+}
