809f945171054ef514b3075ea4af7dfc106fa058
Esteban-L-tech <landaisesteba@gmail.com>
2025-06-27 16:44:55 +0200

    lougout commencement

---
+++ src/Controller/LogoutController.php
@@ -0,0 +1,27 @@
+<?php
+
+// src/Controller/Security/LogoutController.php
+namespace App\Controller\Security;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LogoutController extends AbstractController
+{
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(Request $request, AuthenticationUtils $authenticationUtils): Response
+    {
+        // Récupérer l'utilisateur courant
+        $user = $this->getUser();
+        
+        // Déconnexion manuelle (optionnel)
+        // $this->container->get('security.token_storage')->setToken(null);
+        // $request->getSession()->invalidate();
+        
+        // Redirection vers la page d'accueil
+        return $this->redirectToRoute('app_home');
+    }
+}
