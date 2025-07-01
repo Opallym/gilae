bf54a99495f494091e998c956a4cfb73583a2993
Opallym <gondremaelle@gmail.com>
2025-07-01 12:52:52 +0200

    maj about img dark/light + faq traduction

--- src/Controller/FaqController.php
+++ src/Controller/FaqController.php
@@ -2,18 +2,34 @@
 
 namespace App\Controller;
 
-use App\Service\StatisticsService;
+use App\Repository\FaqRepository;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class FaqController extends AbstractController
 {
     #[Route('/faq', name: 'app_faq')]
-    public function index(StatisticsService $stats): Response
-    {
-        $stats->recordVisit('app_faq');
-        return $this->render('pages/faq/faq.html.twig');
-    }
+    public function index(
+        FaqRepository $faqRepository,
+        RequestStack $requestStack
+    ): Response {
+        $locale = $requestStack->getCurrentRequest()->getLocale();
 
+        $blocs = $faqRepository->findBy(['locale' => $locale]);
+
+        if (!$blocs) {
+            $blocs = $faqRepository->findBy(['locale' => 'fr']);
+        }
+
+        $contenus = [];
+        foreach ($blocs as $bloc) {
+            $contenus[$bloc->getCle()] = $bloc->getContenu();
+        }
+
+        return $this->render('pages/faq/faq.html.twig', [
+            'contenus' => $contenus,
+        ]);
+    }
 }
