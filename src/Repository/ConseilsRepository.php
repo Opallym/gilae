9a0e1de6161e0b9a132067f4228fa795e84462f2
Gwendoline53 <Gwendolinecadet73@gmail.com>
2025-07-03 08:54:15 +0200

    Edit Conseils

--- src/Repository/ConseilsRepository.php
+++ src/Repository/ConseilsRepository.php
@@ -2,18 +2,18 @@
 
 namespace App\Repository;
 
-use App\Entity\Conseil;
+use App\Entity\Conseils;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @extends ServiceEntityRepository<Conseil>
  */
-class ConseilRepository extends ServiceEntityRepository
+class ConseilsRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Conseil::class);
+        parent::__construct($registry, Conseils::class);
     }
 
     // Ici pas besoin de méthode personnalisée : `findBy` suffit !
