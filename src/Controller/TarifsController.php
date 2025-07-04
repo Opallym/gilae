69c59d1c96b2282f27e0f6eb912df3b6fde95a95
Gwendoline53 <Gwendolinecadet73@gmail.com>
2025-07-04 13:39:38 +0200

    Edite Tarifs/Packs et Temoignages/Reçu de temoignages et validation de ceux ci fait / debue de la reception messagerie (a terminer)

--- src/Controller/TarifsController.php
+++ src/Controller/TarifsController.php
@@ -2,18 +2,36 @@
 
 namespace App\Controller;
 
-use App\Service\StatisticsService;
+use App\Repository\TarifsRepository;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class TarifsController extends AbstractController
 {
     #[Route('/tarifs', name: 'app_tarifs')]
-    public function index(StatisticsService $stats): Response
-    {
-        $stats->recordVisit('app_tarifs');
-        return $this->render('pages/tarifs/tarifs.html.twig');
-    }
+    public function index(
+        TarifsRepository $tarifRepository,
+        RequestStack $requestStack
+    ): Response {
+        $locale = $requestStack->getCurrentRequest()->getLocale();
 
+        $blocs = $tarifRepository->findBy(['locale' => $locale]);
+
+        if (!$blocs) {
+            $blocs = $tarifRepository->findBy(['locale' => 'fr']);
+        }
+
+        $contenus = [];
+        foreach ($blocs as $bloc) {
+            $contenus[$bloc->getCle()] = $bloc->getContenu();
+        }
+
+        return $this->render('pages/tarifs/tarifs.html.twig', [
+            'contenus' => $contenus,
+            'mode_edition' => false,
+            'locale' => $locale,
+        ]);
+    }
 }
---
+++ src/Entity/Tarifs.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TarifsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TarifsRepository::class)]
+class Tarifs
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $contenu = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $locale = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $cle = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): static
+    {
+        $this->contenu = $contenu;
+        return $this;
+    }
+
+    public function getLocale(): ?string
+    {
+        return $this->locale;
+    }
+
+    public function setLocale(string $locale): static
+    {
+        $this->locale = $locale;
+        return $this;
+    }
+
+    public function getCle(): ?string
+    {
+        return $this->cle;
+    }
+
+    public function setCle(string $cle): static
+    {
+        $this->cle = $cle;
+        return $this;
+    }
+}
